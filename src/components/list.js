a4badfd7f6081652877f5c197ec1ffef989c0ffd
Vahag <49243454+torosyan1@users.noreply.github.com>
2020-08-16 01:57:45 +0400

    Update list.js

--- src/components/list.js
+++ src/components/list.js
@@ -13,6 +13,7 @@ class List extends React.Component {
         <div className='container'>
           <div className='list'>
             <ul className='list_container'>
+      //is based on the data because I get the data for 6 days and I have printed the current weather:  this.props.data[1][id][0].max_temp
               {this.props.data[0] === undefined ? console.log('empty') : this.props.data[0].map((el, id) => {
                 return <li className='items' key={id}>{el.title}:
                   <span className='temps'> Max: {Math.floor(this.props.data[1][id][0].max_temp)}°C</span>
